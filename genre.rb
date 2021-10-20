5aa45a51e14f31da7fe751dd5cd0769c9d139682
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-10-20 02:40:52 +0100

    feat: Add unit test to check genre and album class

--- genre.rb
+++ genre.rb
@@ -2,7 +2,7 @@ class Genre
   attr_accessor :name
   attr_reader :id, :items
 
-  def initialize(name, _items)
+  def initialize(name)
     @id = Random.rand(1..1000)
     @name = name
     @items = []
